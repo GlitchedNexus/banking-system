e1d0476e22cb649a0b261bc3e575edf8564e2ef4
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2024-05-07 14:40:12 -0700

    Add Sentry for performance metrics and debugging.

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://48ce09b68e7dec7e76bd1e5bb8457202@o4507217175314432.ingest.us.sentry.io/4507217176494080",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
